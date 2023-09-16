0c2d5b7c607842d9dc93c7b8490cc2adbf520f9d
nanstis <bleyo@alphomega.org>
2023-09-16 13:21:42 +0200

    feat: cookies

--- resources/views/common/cookies.blade.php
+++ resources/views/common/cookies.blade.php
@@ -1,30 +1,32 @@
-<div class="cookies-container">
-    <div x-data="cookies" x-show="open" class="cookies">
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-2 my-auto mx-auto">
-                    <img src="{{ asset('images/cookie.svg') }}" alt="Accept cookies" class="my-auto" />
-                </div>
-                <div class="col-10">
-                    <p class="text-start">
-                        {{ __('main.cookies_text') }}
-                    </p>
-                    <div class="d-flex gap-2" aria-label="Cookies">
-                        <button @click="accept" class="btn btn-primary">
-                            {{ __('main.cookies_accept') }}
-                        </button>
-
-                        <a href="{{ url(\App\Http\Middleware\LocaleMiddleware::getLocale() . '/user-terms') }}">
-                            <button type="button" CLASS="btn btn-info">
-                                {{ __('main.cookies_info') }}
+@if (!isset($_COOKIE['cookies-policy']))
+    <div class="cookies-container">
+        <div x-data="cookies" x-show="open" class="cookies">
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col-2 my-auto mx-auto">
+                        <img src="{{ asset('images/cookie.svg') }}" alt="Accept cookies" class="my-auto" />
+                    </div>
+                    <div class="col-10">
+                        <p class="text-start">
+                            {{ __('main.cookies_text') }}
+                        </p>
+                        <div class="d-flex gap-2" aria-label="Cookies">
+                            <button @click="accept" class="btn btn-primary">
+                                {{ __('main.cookies_accept') }}
                             </button>
-                        </a>
+
+                            <a href="{{ url(\App\Http\Middleware\LocaleMiddleware::getLocale() . '/user-terms') }}">
+                                <button type="button" CLASS="btn btn-info">
+                                    {{ __('main.cookies_info') }}
+                                </button>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
+@endif
 
 <script>
     document.addEventListener('alpine:init', () => {
