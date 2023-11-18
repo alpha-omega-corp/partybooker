de1cedcee8c82046cdb19a0e5a2e1d12fd0592ae
nanstis <bleyo@alphomega.org>
2023-11-18 18:30:48 +0100

    fix: vite url

--- resources/views/components/dashboard/profile/category.blade.php
+++ resources/views/components/dashboard/profile/category.blade.php
@@ -19,87 +19,88 @@
                         $advert = Advert::where('partners_info_id', \Illuminate\Support\Facades\Auth::user()->partnerInfo->id)
                         ->where('view_name', $category->form_name)->first();
                     @endphp
+                    <div wire:key="{{$iterator}}">
+                        <div class="category-option-card position-relative mt-3"
+                             x-data="{show: false}">
 
-                    <div class="category-option-card position-relative mt-3"
-                         x-data="{show: false}" wire:key="{{$iterator}}">
+                            <div class="text-uppercase fw-bold text-start w-100 p-2">
+                                {{$category->lang->name}}
+                            </div>
 
-                        <div class="text-uppercase fw-bold text-start w-100 p-2">
-                            {{$category->lang->name}}
-                        </div>
-
-                        <ul class="text-start category-option-ul">
-                            <ul>
-                                @foreach($category->subCategories as $sub)
-                                    <li>{{$sub->lang->name}}</li>
-                                @endforeach
+                            <ul class="text-start category-option-ul">
+                                <ul>
+                                    @foreach($category->subCategories as $sub)
+                                        <li>{{$sub->lang->name}}</li>
+                                    @endforeach
+                                </ul>
                             </ul>
-                        </ul>
 
-                        @if ($advert->status == Advert::STATUS_DRAFT)
-                            <hr>
-                            @switch($advert->view_name)
-                                @case('event-place')
-                                    @break
+                            @if ($advert->status == Advert::STATUS_DRAFT)
+                                <hr>
+                                @switch($advert->view_name)
+                                    @case('event-place')
+                                        @break
 
-                                @case('caterer')
-                                    @php($action = Auth::user()->type == 'admin'
-                                        ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/caterer')
-                                        : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/caterer')
-                                    ) @break
+                                    @case('caterer')
+                                        @php($action = Auth::user()->type == 'admin'
+                                            ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/caterer')
+                                            : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/caterer')
+                                        ) @break
 
-                                @case('wine')
-                                    @php($action = Auth::user()->type == 'admin'
-                                        ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/wine-lovers')
-                                        : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/wine-lovers')
-                                    ) @break
+                                    @case('wine')
+                                        @php($action = Auth::user()->type == 'admin'
+                                            ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/wine-lovers')
+                                            : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/wine-lovers')
+                                        ) @break
 
-                                @case('equipment')
-                                    @php($action = Auth::user()->type == 'admin'
-                                        ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/equipment')
-                                        : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/equipment')
-                                    ) @break
+                                    @case('equipment')
+                                        @php($action = Auth::user()->type == 'admin'
+                                            ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/equipment')
+                                            : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/equipment')
+                                        ) @break
 
-                                @case('entertainment')
-                                    @php($action = Auth::user()->type == 'admin'
-                                        ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/entertainment')
-                                        : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/entertainment')
-                                    ) @break
-                            @endswitch
+                                    @case('entertainment')
+                                        @php($action = Auth::user()->type == 'admin'
+                                            ? url(App\Http\Middleware\LocaleMiddleware::getLocale().'/cp/adverts/'.$advert->id.'/entertainment')
+                                            : url(App\Http\Middleware\LocaleMiddleware::getLocale().'/adverts/'.$advert->id.'/entertainment')
+                                        ) @break
+                                @endswitch
 
-                            <div class="text-center">
-                                <a id="{{'create' . $iterator}}"
+                                <a class="show" id="{{'create' . $iterator}}"
                                    href="{{route('create-' . $advert->view_name, $advert->id)}}">
-                                    Create
+                                    {{__('partner.create_new_service')}}
                                 </a>
-                            </div>
-                        @else
 
-                            <hr>
+                            @else
 
-                            <div x-show="show">
-                                <x-partner.service-details :advert="$advert" :partner="auth()->user()->partnerInfo"
-                                                           :iterator="$iterator"/>
-                            </div>
+                                <hr>
 
-                            <a class="show" @click="show = !show">
+                                <div x-show="show">
+                                    <x-partner.service-details :advert="$advert" :partner="auth()->user()->partnerInfo"
+                                                               :iterator="$iterator"/>
+                                </div>
+
+                                <a class="show" @click="show = !show">
                                 <span x-show="!show">
                                     {{__('form.show')}}
                                 </span>
 
-                                <span x-show="show">
+                                    <span x-show="show">
                                     {{__('form.hide')}}
                                 </span>
-                            </a>
-
-                            <div class="edit-service">
-                                <a id="{{'create' . $iterator}}"
-                                   href="{{route('create-' . $advert->view_name, $advert->id)}}">
-                                    @svg('heroicon-o-cog-6-tooth')
                                 </a>
-                            </div>
 
-                        @endif
+                                <div class="edit-service">
+                                    <a id="{{'create' . $iterator}}"
+                                       href="{{route('create-' . $advert->view_name, $advert->id)}}">
+                                        @svg('heroicon-o-cog-6-tooth')
+                                    </a>
+                                </div>
+
+                            @endif
+                        </div>
                     </div>
+
 
                     @if(!$loop->last)
                         <hr>
