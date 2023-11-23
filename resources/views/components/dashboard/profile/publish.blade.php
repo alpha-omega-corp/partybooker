286c2c65c869a8fc7905a73f6f277b7c73dd194b
nanstis <bleyo@alphomega.org>
2023-11-23 13:48:11 +0100

    fix: publish

--- resources/views/components/dashboard/profile/publish.blade.php
+++ resources/views/components/dashboard/profile/publish.blade.php
@@ -8,17 +8,17 @@
     <x-slot:badge>
         @if ($partner->public == 0)
             <span
-                    class="status-badge badge text-bg-danger text-white">{{ __('partner.not_published') }}</span>
+                class="status-badge badge text-bg-danger text-white">{{ __('partner.not_published') }}</span>
         @else
             <span
-                    class="status-badge badge text-bg-primary text-white">{{ __('partner.published') }}</span>
+                class="status-badge badge text-bg-primary text-white">{{ __('partner.published') }}</span>
         @endif
     </x-slot:badge>
 
     @if($service->canPublish($partner->id_partner) || \Illuminate\Support\Facades\Auth::user()->type === 'admin')
         <div class="d-flex flex-column m-0">
             @if ($partner->public == 0)
-                <a href="#" class="status-button text-primary shadow-lg" role="button"
+                <a class="status-button text-primary shadow-lg" role="button"
                    data-id="{{ $partner->id_partner }}">
                     {{ __('partner.publish')}}
                 </a>
