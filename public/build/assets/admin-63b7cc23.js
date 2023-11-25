4cfa630586798b50301ba5bb46983b5e11ddda44
nanstis <bleyo@alphomega.org>
2023-11-25 11:51:47 +0100

    fix: manifest

---
+++ public/build/assets/admin-63b7cc23.js
@@ -0,0 +1,1 @@
+import{m as a,b as d}from"./module.esm-f7c3d697.js";window.Alpine=a;a.plugin(d);a.start();
--- public/build/assets/admin-bd6c7a50.js
+++
@@ -1,1 +0,0 @@
-import{m as a,b as d}from"./module.esm-53627d7c.js";window.Alpine=a;a.plugin(d);a.start();
