b6db9b76685e040917b317e0e9fb2768b973cd0e
nanstis <bleyo@alphomega.org>
2023-11-27 18:52:10 +0100

    test

--- resources/views/admin/listing.blade.php
+++ resources/views/admin/listing.blade.php
@@ -8,73 +8,8 @@
     <title>{{ __('partybooker-cp.listing')}} | {{ __('partybooker-cp.www')}}</title>
 @endsection
 
-@section('content')
-    <div class="container mt-5">
-        <div class="listing-title">
-            <x-partner-category-tab
-                :tabs="$planTabs">
 
-
-                <x-tab.item>
-                    <x-partner.admin-listing
-                        plan="basic"
-                        :partners="$basicPartners"/>
-                </x-tab.item>
-
-                <x-tab.item>
-                    <x-partner.admin-listing
-                        plan="commission"
-                        :partners="$commissionPartners"/>
-                </x-tab.item>
-
-                <x-tab.item>
-                    <x-partner.admin-listing
-                        plan="standart"
-                        :partners="$standardPartners"/>
-                </x-tab.item>
-
-                <x-tab.item>
-                    <x-partner.admin-listing
-                        plan="premium"
-                        :partners="$premiumPartners"/>
-                </x-tab.item>
-
-                <x-tab.item>
-                    <x-partner.admin-listing
-                        plan="premium"
-                        :partners="$premiumPartners"/>
-                </x-tab.item>
-
-            </x-partner-category-tab>
-        </div>
-    </div>
-
-
-    <script type="module">
-        $(document).on('click', 'a#remove-partner', function (e) {
-            var $this = $(this);
-            if (confirm('Do yo want to delete this partner?')) {
-                $.ajax({
-                    url: '/cp/partner-remove',
-                    method: 'post',
-                    data: {
-                        id_partner: $(this).data('partner')
-                    },
-                    success: function () {
-                        $this.closest('div.partner-box').remove();
-                    },
-                    error: function (r) {
-                        var data = JSON.parse(r.responseText);
-                        alert(data.message);
-                    }
-                })
-            }
-            return false;
-        });
-    </script>
-@endsection
-
-@push('footer')
+@push('head')
     <script>
         function searchHandler() {
             return {
@@ -175,3 +110,69 @@
         }
     </script>
 @endpush
+
+@section('content')
+    <div class="container mt-5">
+        <div class="listing-title">
+            <x-partner-category-tab
+                :tabs="$planTabs">
+
+
+                <x-tab.item>
+                    <x-partner.admin-listing
+                        plan="basic"
+                        :partners="$basicPartners"/>
+                </x-tab.item>
+
+                <x-tab.item>
+                    <x-partner.admin-listing
+                        plan="commission"
+                        :partners="$commissionPartners"/>
+                </x-tab.item>
+
+                <x-tab.item>
+                    <x-partner.admin-listing
+                        plan="standart"
+                        :partners="$standardPartners"/>
+                </x-tab.item>
+
+                <x-tab.item>
+                    <x-partner.admin-listing
+                        plan="premium"
+                        :partners="$premiumPartners"/>
+                </x-tab.item>
+
+                <x-tab.item>
+                    <x-partner.admin-listing
+                        plan="premium"
+                        :partners="$premiumPartners"/>
+                </x-tab.item>
+
+            </x-partner-category-tab>
+        </div>
+    </div>
+
+    <script type="module">
+        $(document).on('click', 'a#remove-partner', function (e) {
+            var $this = $(this);
+            if (confirm('Do yo want to delete this partner?')) {
+                $.ajax({
+                    url: '/cp/partner-remove',
+                    method: 'post',
+                    data: {
+                        id_partner: $(this).data('partner')
+                    },
+                    success: function () {
+                        $this.closest('div.partner-box').remove();
+                    },
+                    error: function (r) {
+                        var data = JSON.parse(r.responseText);
+                        alert(data.message);
+                    }
+                })
+            }
+            return false;
+        });
+    </script>
+@endsection
+
