f2d8c68bfd2fee830357bd101f932009b9697e82
nanstis <bleyo@alphomega.org>
2023-11-29 11:18:19 +0100

    fix

--- resources/views/components/partner/listing.blade.php
+++ resources/views/components/partner/listing.blade.php
@@ -74,7 +74,7 @@
                             <div class="col-md-8">
                                 <div class="card-body">
                                     <h5 class="card-title text-uppercase fw-bold listing-card-title"
-                                        x-text="unEscape(partner.company)">
+                                        x-text="$('<div/>').text(unEscape(partner.company)).html()">
 
                                     </h5>
                                     <div class="card-text description" x-text="partner.shortDescription">
