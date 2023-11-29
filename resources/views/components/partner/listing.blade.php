12ac52f7231fbfb7518d7adbc730ee17a74476f6
nanstis <bleyo@alphomega.org>
2023-11-29 11:38:16 +0100

    fix: listing hreff

--- resources/views/components/partner/listing.blade.php
+++ resources/views/components/partner/listing.blade.php
@@ -15,7 +15,7 @@
                 'company' => app()->getLocale() === 'en'
                 ? str_replace(["'"], '', $partner->en_company_name)
                 : str_replace("'", '', $partner->fr_company_name),
-                'href' => url(LocaleMiddleware::getLocale() . '/' . __('urls.listings') . '/' . str_replace("'", '', $partner->slug)),
+                'href' => url(LocaleMiddleware::getLocale() . '/' . __('urls.listing') . '/' . str_replace("'", '', $partner->slug)),
                 'shortDescription' => app()->getLocale() === 'en'
                 ? htmlspecialchars(str_replace(["'", "\r", "\n", ","], '', $partner->en_short_descr))
                 : htmlspecialchars(str_replace(["'", "\r", "\n", ","], '', $partner->fr_short_descr)),
