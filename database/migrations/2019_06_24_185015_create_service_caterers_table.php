2f5e4e7abbc47c8a13cd9fcbf8f178eecfdf0d95
nanstis <bleyo@alphomega.org>
2024-03-29 21:58:20 +0100

    fix: caterer service migration

--- database/migrations/2019_06_24_185015_create_service_caterers_table.php
+++ database/migrations/2019_06_24_185015_create_service_caterers_table.php
@@ -9,7 +9,6 @@ return new class extends Migration {
     {
         Schema::create('service_caterers', function (Blueprint $table) {
             $table->increments('id');
-            $table->json('menus');
             $table->timestamps();
         });
     }
