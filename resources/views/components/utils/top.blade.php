ae9fc21678ac0c5243a90dd365ab4b9b2ac37f50
nanstis <bleyo@alphomega.org>
2024-04-29 13:35:46 +0200

    fix: top service empty advert

--- resources/views/components/utils/top.blade.php
+++ resources/views/components/utils/top.blade.php
@@ -5,18 +5,21 @@
 @if($partner)
     @php($advert = $partner->company->adverts()->main()->first())
 
-    <a class="top-card" href="{{route(__('route.advert'), [
-        'company' => $partner->company,
-        'advert' => $advert,
-    ])}}">
-        <img src="{{$advert->images()->thumbnail()->first()->path}}"
-             alt="{{$partner->company->name}}"/>
+    @if($advert)
+        <a class="top-card" href="{{route(__('route.advert'), [
+            'company' => $partner->company,
+            'advert' => $advert,
+        ])}}">
+            <img src="{{$advert->images()->thumbnail()->first()->path}}"
+                 alt="{{$partner->company->name}}"/>
 
-        <h6 class="top-card-title">{{$partner->company->name}}</h6>
+            <h6 class="top-card-title">{{$partner->company->name}}</h6>
 
-        <div class="top-card-content">
-            <span>{{$partner->company->location->address}}</span>
-            <span>{{$partner->company->loc}}</span>
-        </div>
-    </a>
+            <div class="top-card-content">
+                <span>{{$partner->company->location->address}}</span>
+                <span>{{$partner->company->loc}}</span>
+            </div>
+        </a>
+    @endif
+
 @endif
