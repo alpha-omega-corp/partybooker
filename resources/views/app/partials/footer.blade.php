7c718ad26d7933fa273d8cb0419e7b81f036c6e7
nanstis <bleyo@alphomega.org>
2024-05-07 13:08:39 +0200

    fix: category height

--- resources/views/app/partials/footer.blade.php
+++ resources/views/app/partials/footer.blade.php
@@ -3,28 +3,6 @@
         <div class="app-footer-nav">
             <div class="app-footer-links">
                 <div class="app-footer-link">
-                    <h4 class="app-footer-title">{{__('home.sitemap')}}</h4>
-
-                    <div class="app-footer-list">
-                        <a href="{{route(__('route.home'))}}">{{__('nav.home')}}</a>
-                        <a href="{{route(__('route.listing'))}}">{{__('nav.listing')}}</a>
-                        <a href="{{route(__('route.about'))}}">{{__('nav.about')}}</a>
-                        <a href="{{route(__('route.partnership'))}}">{{__('nav.partnership')}}</a>
-                        <a href="{{route(__('route.blog'))}}">{{__('nav.blog')}}</a>
-                        <a href="{{route(__('route.faq'))}}">{{__('nav.faq')}}</a>
-                    </div>
-                </div>
-
-                <div class="app-footer-link">
-                    <h4 class="app-footer-title">{{__('home.footer')}}</h4>
-                    <div class="app-footer-list">
-                        <a href="{{route(__('route.sitemap'))}}">{{__('nav.sitemap')}}</a>
-                        <a href="{{route(__('route.terms'))}}">{{__('nav.terms')}}</a>
-                        <a href="{{route(__('route.contact'))}}">{{__('nav.contact')}}</a>
-                    </div>
-                </div>
-
-                <div class="app-footer-link">
                     <h4 class="app-footer-title">{{__('home.categories')}}</h4>
 
                     <div class="app-footer-list">
@@ -37,30 +15,42 @@
                 </div>
 
                 <div class="app-footer-link">
-                    <h4 class="app-footer-title">{{__('home.contacts')}}</h4>
+                    <h4 class="app-footer-title">{{__('home.sitemap')}}</h4>
 
-                    <div class="app-footer-list app-footer-contacts">
-                        <p>{{$footerContacts->email}}</p>
-                        <p>{{$footerContacts->phone}}</p>
-                        <div class="flex-column gap-2 d-flex justify-content-between mb-3">
-                            <a href="{{$footerContacts->instagram}}">
-                                instagram
-                            </a>
-                            <a href="{{$footerContacts->facebook}}">
-                                facebook
-                            </a>
-                            <a href="{{$footerContacts->linkedin}}">
-                                linkedin
-                            </a>
-
-                        </div>
-
-                        <p>{{$footerContacts->address}}</p>
-
+                    <div class="app-footer-list">
+                        <a href="{{route(__('route.sitemap'))}}">{{__('nav.sitemap')}}</a>
+                        <a href="{{route(__('route.terms'))}}">{{__('nav.terms')}}</a>
+                        <a href="{{route(__('route.contact'))}}">{{__('nav.contact')}}</a>
+                        <a href="{{route(__('route.home'))}}">{{__('nav.home')}}</a>
+                        <a href="{{route(__('route.listing'))}}">{{__('nav.listing')}}</a>
+                        <a href="{{route(__('route.about'))}}">{{__('nav.about')}}</a>
+                        <a href="{{route(__('route.partnership'))}}">{{__('nav.partnership')}}</a>
+                        <a href="{{route(__('route.blog'))}}">{{__('nav.blog')}}</a>
+                        <a href="{{route(__('route.faq'))}}">{{__('nav.faq')}}</a>
                     </div>
                 </div>
 
+                <div class="app-footer-link">
+                    <h4 class="app-footer-title">{{__('home.contacts')}}</h4>
 
+                    <div class="app-footer-list">
+                        <p>{{$footerContacts->email}}</p>
+                        <p>{{$footerContacts->phone}}</p>
+                        <div class="flex-column gap-2 d-flex justify-content-between mb-3">
+                            <a href="{{$footerContacts->instagram}}" target="_blank">
+                                instagram
+                            </a>
+                            <a href="{{$footerContacts->facebook}}" target="_blank">
+                                facebook
+                            </a>
+                            <a href="{{$footerContacts->linkedin}}" target="_blank">
+                                linkedin
+                            </a>
+                        </div>
+
+                        <p>{{$footerContacts->address}}</p>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
