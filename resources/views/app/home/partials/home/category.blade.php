69ae0ba754b58a1a771c24ac41f675489f37722e
nanstis <bleyo@alphomega.org>
2024-05-16 18:34:35 +0200

    fix: section inside

--- resources/views/app/home/partials/home/category.blade.php
+++ resources/views/app/home/partials/home/category.blade.php
@@ -1,20 +1,20 @@
 <div class="home-category-container">
     @foreach ($categories as $category)
-        <section>
-            @php
-                $icon = match ($category->service) {
-                    CategoryType::EVENT->value => 'heroicon-s-map-pin',
-                    CategoryType::CATERER->value => 'heroicon-s-cake',
-                    CategoryType::WINE->value => 'heroicon-s-trophy',
-                    CategoryType::EQUIPMENT->value => 'heroicon-s-shopping-cart',
-                    CategoryType::ENTERTAINMENT->value => 'heroicon-s-musical-note',
-                };
-            @endphp
 
-            <a href="{{route(__('route.listing'), [
+        @php
+            $icon = match ($category->service) {
+                CategoryType::EVENT->value => 'heroicon-s-map-pin',
+                CategoryType::CATERER->value => 'heroicon-s-cake',
+                CategoryType::WINE->value => 'heroicon-s-trophy',
+                CategoryType::EQUIPMENT->value => 'heroicon-s-shopping-cart',
+                CategoryType::ENTERTAINMENT->value => 'heroicon-s-musical-note',
+            };
+        @endphp
+
+        <a href="{{route(__('route.listing'), [
                 'category' => $category->locale->slug,
             ])}}">
-
+            <section>
                 <div class="home-category-card shadow-lg">
                     <div class="category-card-icon">
                         @svg($icon)
@@ -23,7 +23,8 @@
                         <h6 class="category-card-title">{{$category->locale->title}}</h6>
                     </div>
                 </div>
-            </a>
-        </section>
+            </section>
+        </a>
+
     @endforeach
 </div>
