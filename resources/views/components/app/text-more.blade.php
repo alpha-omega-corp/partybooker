c76bdea1d1ccae244d8c83386f3dc46b925db064
nanstis <bleyo@alphomega.org>
2024-05-16 12:39:29 +0200

    fix: home layout shift

--- resources/views/components/app/text-more.blade.php
+++ resources/views/components/app/text-more.blade.php
@@ -2,7 +2,7 @@
 
 <div x-data="truncate('{{json_encode(str_replace("'", '', $content))}}')">
     <div class="p-4" x-html="truncated"></div>
-    <button type="button" class="btn" x-show="canTruncate" @click="expand()">
+    <button type="button" class="btn p-4" x-show="canTruncate" @click="expand()">
         <span x-show="expanded">{{__('advert.close')}}</span>
         <span x-show="!expanded">{{ucfirst(__('app.mobile-more'))}}</span>
     </button>
